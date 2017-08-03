aa48be67a0a4614be27404c2cf01fd1f4babfae6
Alexander Tsimbalyuk <a.tsimbalyuk1@gmail.com>
2017-08-03 23:07:39 +0300

    Made some edits and adde a play timer

--- scripts.js
+++ scripts.js
@@ -1,15 +1,18 @@
-var player = document.querySelector(".player");
+var player = document.querySelector('.player');
 	
-var button = document.querySelectorAll(".track__button");
-
 var track = new Audio();
-
 var field;
+var minutes;
+var seconds;
+var time = 0;
+var interval;
 
 player.onclick = function(event) {
 	var target = event.target; 
 	var sound = target.nextElementSibling.dataset.sound;
 	field = target.nextElementSibling;
+	minutes = field.children[2].children[0];
+	seconds = field.children[2].children[1];
 	
 	while (target != this) {  
 		if (target.className == 'track__button' || target.className == 'track__button active') { 
@@ -21,10 +24,10 @@ player.onclick = function(event) {
 };
 
 function playTrack(sound, press){
-	
 	if (track.currentTime == 0 || track.paused){
 		track.src = sound;
 		track.play();
+		start();
 		press.classList.add('active');
 		playtimeAnimate(field, track);
 		
@@ -32,6 +35,7 @@ function playTrack(sound, press){
 			if (track.ended){ 
 				setTimeout(function(){	
 					press.classList.remove('active');
+					getReset();
 					clearInterval(timerId);
 				},500);
 			}
@@ -42,6 +46,7 @@ function playTrack(sound, press){
 		track.pause();
 		track.currentTime == 0;
 		press.classList.remove('active');
+		getReset();
 		clearInterval(timerId);
 	}
 }
@@ -54,15 +59,46 @@ function playtimeAnimate(activeField, track){
 		
 		var pct = currentTime / ( duration / 100 );
 		
-		activeField.style.boxShadow = "inset " + activeField.clientWidth * ( pct / 100 ) + "px 0 0 0 rgba(255, 204, 51, 1)";
-		activeField.style.transition = "0.5s";
+		activeField.style.boxShadow = 'inset ' + activeField.clientWidth * ( pct / 100 ) + "1px -1px 1px 1px rgba(255, 204, 51, 1)";
+		activeField.style.transition = '0.5s';
 		
 		if (track.ended || track.paused){
 			setTimeout(function(){
-				activeField.style.boxShadow = "none";
-				activeField.style.transition = "0.25s";
+				activeField.style.boxShadow = 'none';
+				activeField.style.transition = '0.25s';
 				clearInterval(timerId);
 			},500);
 		}
 	}, 100);
+}
+
+function start(){
+	interval = setInterval(incrementTime, 1000);
+}
+
+function incrementTime(){
+	time++;
+
+	var MIN = Math.floor(time / 60);
+	var SEC = time % 60;
+
+	minutes.innerText = fill(MIN);
+	seconds.innerText = fill(SEC);
+}
+
+function getReset(){
+	clearInterval(interval);
+
+	time = 0;
+
+	minutes.innerText = '00';
+	seconds.innerText = '00';
+}
+
+function fill(x){
+	if (x < 10){
+		return '0' + x;
+	} else {
+		return x;
+	}
 }
